5bea4f48d3e10e3268fa24a14ab1540c474d202f
alekmatyka <amatyka@student.agh.edu.pl>
2024-12-09 17:57:03 +0100

    setup

--- Projekt/src/main/java/projekt/DarwinWorld.java
+++ Projekt/src/main/java/projekt/DarwinWorld.java
@@ -1,7 +1,12 @@
 package projekt;
+
+import projekt.model.Vector2d;
+
 public class DarwinWorld {
 
     public static void main(String[] args) {
         System.out.println("działa");
+        Vector2d v = new Vector2d(1,1);
+        System.out.println(v);
     }
 }
