cd8f8e41218733c93b2abb84d6d6ecf6ffafdf75
alekmatyka <amatyka@student.agh.edu.pl>
2025-01-16 23:29:16 +0100

    kolorowanie na czerwono

--- Projekt/src/main/java/projekt/model/Statistics.java
+++ Projekt/src/main/java/projekt/model/Statistics.java
@@ -3,14 +3,20 @@ package projekt.model;
 import java.util.List;
 
 public record Statistics(int animalCount, int plantCount, int freeSpacesCount,
-                         String popularGenes, double averageEnergy,
+                         List<List<Integer>> popularGenes, double averageEnergy,
                          double averageLifespan, double averageChildCount) {
     @Override
     public String toString() {
+
+        String popGenes = popularGenes.stream()
+                .map(gene -> gene.toString() + "\n")
+                .reduce((a,b)->a+b)
+                .orElse("");
+
         return  "ilosc zwierzat \t" + animalCount +
                 "\nilosc roslin \t" + plantCount +
                 "\nwolnych pol \t" + freeSpacesCount +
-                "\ntopowe genomy:\n" + popularGenes + '\'' +
+                "\ntopowe genomy:\n" + popGenes + '\'' +
                 "\nsrednia energia \t" + (double)(Math.round(averageEnergy*100))/100.00 +
                 "\nsredni zywot \t" + (double)(Math.round(averageLifespan*100))/100.00 +
                 "\nsrednia dzietnosc \t" + (double)(Math.round(averageChildCount*100))/100.00;
