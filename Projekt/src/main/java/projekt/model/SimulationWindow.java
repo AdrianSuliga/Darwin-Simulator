4b76104d72e636f487521656893f1bd72e755168
alekmatyka <amatyka@student.agh.edu.pl>
2025-01-18 11:40:36 +0100

    ui improvments

--- Projekt/src/main/java/projekt/model/SimulationWindow.java
+++ Projekt/src/main/java/projekt/model/SimulationWindow.java
@@ -23,6 +23,14 @@ public class SimulationWindow implements MapChangeListener {
     private final VBox statLayout;
 
     private final HBox trackedAnimalStatsLayout;
+    private final Label animalCountLabel;
+    private final Label freeSpaceLabel;
+    private final Label plantCountLabel;
+    private final VBox mapStatsLayout;
+    private final Label topGenomesLabel;
+    private final Label averageEnergyLabel;
+    private final Label averageLifespanLabel;
+    private final Label averageChildCountLabel;
 
 
     private Label statLabel;
@@ -67,13 +75,26 @@ public class SimulationWindow implements MapChangeListener {
 
 
         HBox buttons = new HBox(stopButton,continueButton);
+        buttons.setStyle("-fx-maxheigth: 50px; -fx-alignment: center;");
 
         //staty
-        statLabel = new Label("Statistics");
+        statLabel = new Label("Statystyki");
+        animalCountLabel = new Label("Ilosc zwierzat");
+        plantCountLabel = new Label("Ilosc roslin");
+        freeSpaceLabel = new Label("Ilosc wolnych pol");
+        topGenomesLabel = new Label("Genomy");
+        averageEnergyLabel = new Label("Srednia energia");
+        averageLifespanLabel = new Label("Sredni zywot");
+        averageChildCountLabel = new Label("Srednia dzietnosc");
+        mapStatsLayout = new VBox(10,statLabel,animalCountLabel,
+                plantCountLabel,freeSpaceLabel,topGenomesLabel,
+                averageEnergyLabel,averageLifespanLabel,averageChildCountLabel);
+        mapStatsLayout.setStyle(" -fx-padding: 10;-fx-border-color: black; -fx-border-width: 3px; -fx-border-radius: 10px; -fx-margin: 10px");
+
         animalStatLabel = new Label();
         trackedAnimalStatsLayout = new HBox(10,animalStatLabel);
-        statLayout = new VBox(10,statLabel,trackedAnimalStatsLayout);
-        statLayout.setStyle("-fx-padding: 20; -fx-alignment: center;");
+        statLayout = new VBox(10,buttons,mapStatsLayout,trackedAnimalStatsLayout);
+        statLayout.setStyle("-fx-padding: 20; -fx-alignment: center; ");
 
         //mapa
 
@@ -84,17 +105,16 @@ public class SimulationWindow implements MapChangeListener {
         this.simulation.getWorldMap().registerObserver(this);
         this.maxX = simulation.getWorldMap().getWidth() - 1;
         this.maxY = simulation.getWorldMap().getHeight() - 1;
-        this.cell_width = 500 / maxX;
-        this.cell_height = 500 / maxY;
+        this.cell_width = (int) Math.floor(500.0 / ((Math.max(this.maxX,this.maxY)+1)));
+        this.cell_height = this.cell_width;
         VBox layout = new VBox(10, mainGrid);
         layout.setStyle("-fx-alignment: center;");
         //zawiera wszystko
         BorderPane pane = new BorderPane();
-        pane.setTop(buttons);
         pane.setLeft(statLayout);
         pane.setCenter(layout);
 
-        Scene scene = new Scene(pane, 2 * maxX * cell_width, 2 * maxY * cell_height);
+        Scene scene = new Scene(pane, (2 * maxX * cell_width)+200, (2 * maxY * cell_height)+200);
         stage.setScene(scene);
         stage.setTitle("Simulation");
         stage.setOnCloseRequest(windowEvent -> stopSimulationInNewWindow());
@@ -119,9 +139,7 @@ public class SimulationWindow implements MapChangeListener {
     public void mapChanged(WorldMap map) {
 
         synchronized (map) {
-            map.updateStatistics();
             statistics=map.getStatistics();
-            mapStats=statistics.toString();
             this.animalMap = new HashMap<>(map.getAnimalMap());
             this.plantList = new HashMap<>(map.getPlantList());
         }
@@ -131,9 +149,9 @@ public class SimulationWindow implements MapChangeListener {
     public void doWork(){
         try {
             Platform.runLater(() -> {
-            statLabel.setText(mapStats);
-            this.drawMap();
-            this.drawTrackedAnimal();
+                this.drawStatistics();
+                this.drawMap();
+                this.drawTrackedAnimal();
             });
 
             synchronized (this) {
@@ -149,9 +167,26 @@ public class SimulationWindow implements MapChangeListener {
         }
     }
 
+    private void drawStatistics(){
+        this.animalCountLabel.setText("Ilosc zwierzat: "+statistics.animalCount());
+        this.plantCountLabel.setText("Ilosc roslin: "+statistics.plantCount());
+        this.freeSpaceLabel.setText("Ilosc wolnych pol: "+statistics.freeSpacesCount());
+        this.averageEnergyLabel.setText("Srednia energia: "+(double) Math.round(statistics.averageEnergy()*100)/100);
+        this.averageLifespanLabel.setText("Sredni zywot: "+(double) Math.round(statistics.averageLifespan()*100)/100);
+        this.averageChildCountLabel.setText("Srednia dzietnosc: "+(double) Math.round(statistics.averageChildCount()*100)/100);
+        String popularGenomes = "Topowe genomy\n";
+        for(int i=0;i<statistics.popularGenes().size();i++){
+            popularGenomes = popularGenomes + (i+1) +". "+ statistics.popularGenes().get(i)+'\n';
+        }
+        this.topGenomesLabel.setText(popularGenomes);
+    }
     private void drawTrackedAnimal(){
         if(trackedAnimal!=null){
             this.animalStatLabel.setText(trackedAnimal.getStatistics());
+            //trackedAnimalStatsLayout.setStyle("-fx-padding: 10;-fx-border-color: black; -fx-border-width: 3px; -fx-border-radius: 10px; -fx-margin: 10px");
+        } else {
+            //trackedAnimalStatsLayout.setStyle("");
+
         }
 
     }
@@ -208,7 +243,7 @@ public class SimulationWindow implements MapChangeListener {
             for (int j = maxY; j >= 0; j--) {
                 Vector2d pos = new Vector2d(i, j);
                 Label label;
-                if (this.animalMap.containsKey(pos) && !this.animalMap.get(pos).isEmpty()) {
+                if (this.animalMap.containsKey(pos) && this.animalMap.get(pos)!=null  && !this.animalMap.get(pos).isEmpty()) {
                     label = new Label("X");
                 } else if (this.plantList.containsKey(pos)) {
                     label = new Label(this.plantList.get(pos).toString());
